170bdc22a8482eb27c9f358d7507af77f7a33042
Dandiggas <dadekugbe@googlemail.com>
2024-05-03 23:55:03 +0100

    add and print

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use clap::{App, AppSettings, Arg, ArgMatches};
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+use std::fs::OpenOptions;
+use std::fs::read_to_string;
+use std::io::Write;
 
-// Edith has heard about a cool new task manager CLI app.
-// She goes to check out its help message by running the app with the --help flag.
-// $ task_manager --help
-
-// She is invited to add a new task using the `add` subcommand.
-// She types "Buy peacock feathers" as the task title.
-// (Edith's hobby is tying fly-fishing lures)
+#[derive(Serialize, Deserialize)]
+struct Task {
+    id: i32,
+    task: String,
+    completed: bool,
+}
 
 fn main() {
     let matches = App::new("Task Manager")
@@ -24,8 +28,8 @@ fn main() {
 
     match matches.subcommand() {
         Some(("add", sub_matches)) => {
-            let task = sub_matches.value_of("TASK").unwrap();
-            println!("Task added: {}", task)
+            let task_title = sub_matches.value_of("TASK").unwrap();
+            let _ = print_a_task(task_title); 
         }
         Some(("list", _)) => {
             // Handle the "list" subcommand
@@ -39,3 +43,62 @@ fn main() {
         _ => unreachable!(),
     }
 }
+
+
+
+pub fn print_a_task(task_title: &str) -> Result<String> {
+
+    let task = Task {
+        id: 1,
+        task: task_title.to_owned(),
+        completed: false,
+    };
+
+    // Serialize it to a JSON string.
+    let j = serde_json::to_string_pretty(&task)?;
+
+    // Print, write to a file, or send to an HTTP server.
+    println!("{}", j);
+
+    Ok(j)
+}
+// pub fn write_to_file(j: String) {
+//     // check is the file exists if not create
+//     let file = OpenOptions::new()
+//         .write(true)
+//         .create_new(true)
+//         .open("task.json");
+
+//     if let Ok(mut file) = file {
+//         if let Err(e) = file.write_all(j.as_bytes()) {
+//             eprintln!("Couldn't write to file: {}", e);
+//         } else {
+//             println!("Successfully wrote to the file.")
+//         }
+//     } else {
+//         eprintln!("File already exists");
+//     }
+// }
+
+// fn deserialize_and_print() -> Result<()> {
+//     // Some JSON input data as a &str. Maybe this comes from the user.
+//     let data = read_to_string("task.json").unwrap();
+
+//     // Parse the string of data into a Person object. This is exactly the
+//     // same function as the one that produced serde_json::Value above, but
+//     // now we are asking it for a Person as output.
+//     let t: Task = serde_json::from_str(&data).unwrap();
+
+//     // Do things just like with any other Rust data structure.
+//     println!("Task ID: {} Task: {}", t.id, t.task);
+
+//     Ok(())
+// }
+
+// fn main()-> Result<()>{
+//     let j = print_a_task()?;
+
+//     write_to_file(j);
+//     deserialize_and_print()?;
+//  Ok(())
+// }
